e69ac7b7ac5fcc8f4c4ce8271134e468ea8b4f94
droidtechnician <droidtechnicians@gmail.com>
2018-09-03 19:16:14 +0530

    URL issue fixed

--- src/app/constants/url.const.ts
+++ src/app/constants/url.const.ts
@@ -1,6 +1,6 @@
 import { environment } from '../../environments/environment';
 
 export const url = {
-    login: environment.baseUri + '/login',
-    signup: environment.baseUri + '/signup'
+    login: environment.baseUri + 'login',
+    signup: environment.baseUri + 'signup'
 };
